006cfb945ea1cf0725df0278509593aae046b32e
Florian Fischer <florian.fl.fischer@fau.de>
2015-07-13 13:37:05 +0200

    add screensaver and dpms toogle functions

---
+++ config/fish/functions/screensoff.fish
@@ -0,0 +1,3 @@
+function screensoff
+	xset s off; xset -dpms; and echo 'xcreensaver and dpms disabled' $argv; 
+end
---
+++ config/fish/functions/screenson.fish
@@ -0,0 +1,3 @@
+function screenson
+	xset s on; xset +dpms; and echo 'xscreensaver and dpms enabled' $argv; 
+end
