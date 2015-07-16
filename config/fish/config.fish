203409d662033ac036b9855f7194e4fb4bcc42cb
Florian Fischer <florian.fl.fischer@fau.de>
2015-07-16 12:53:00 +0200

    make sure __fish_git_prompt_show_informative_status is set.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,2 +1,7 @@
 # add git-alias subdirectory to functions path
 set fish_function_path /home/fischerling/.config/fish/functions/git-alias $fish_function_path
+
+if not set -q __fish_git_prompt_show_informative_status
+    set -Ux __fish_git_prompt_show_informative_status true
+end
+_
