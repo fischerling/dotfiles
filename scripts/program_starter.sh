2a9bfb8a3c90031cea661b1de69caca885b21530
Florian Fischer <florian.fl.fischer@fau.de>
2015-11-25 18:46:48 +0100

    improve starter scripts
    1) program starter print error if rofi and dmenu aren't available
    2) ssh starter also is usable with dmenu

--- scripts/program_starter.sh
+++ scripts/program_starter.sh
@@ -3,6 +3,9 @@
 if type rofi &>/dev/null
 then
     exec rofi -show run
-else
+elif type dmenu_run &>/dev/null
+then
     exec dmenu_run
+else
+    i3-nagbar -m "please install rofi or dmenu to start a program" &>/dev/null
 fi
--- scripts/ssh_starter.sh
+++ scripts/ssh_starter.sh
@@ -3,6 +3,10 @@
 if type rofi &>/dev/null
 then
     exec rofi -show ssh
+elif type dmenu &>/dev/null
+then
+    
+    exec i3-sensible-terminal -e ssh $(grep Host ~/.ssh/config | cut -f2 -d' ' | dmenu)
 else
-    exec i3-nagbar -m "please install rofi to use the ssh starter" &>/dev/null
+    exec i3-nagbar -m "please install rofi or dmenu to use the ssh starter" &>/dev/null
 fi
