5bb4bbb0f3c86767c68969bf61881ed89abc3a43
Florian Fischer <florian.fl.fischer@fau.de>
2015-12-01 18:55:37 +0100

    set visualbell for ircbox termnals
    1) let rofi only print the ssh command
    2) start urxvt with -vb if rofi's output contains ircbox

--- scripts/ssh_starter.sh
+++ scripts/ssh_starter.sh
@@ -2,11 +2,21 @@
 
 if type rofi &>/dev/null
 then
-    exec rofi -show ssh
+    cmd=$(rofi -show ssh)
 elif type dmenu &>/dev/null
 then
     
-    exec i3-sensible-terminal -e ssh $(grep Host ~/.ssh/config | cut -f2 -d' ' | dmenu)
+    cmd="ssh $(grep Host ~/.ssh/config | cut -f2 -d' ' | dmenu)"
 else
     exec i3-nagbar -m "please install rofi or dmenu to use the ssh starter" &>/dev/null
 fi
+
+
+if [[ $cmd == *"ircbox"* ]] && [[ $TERMINAL == "urxvt" ]]
+then
+    exec i3-sensible-terminal -vb -e $cmd
+
+else
+    exec i3-sensible-terminal -e $cmd
+fi
+
