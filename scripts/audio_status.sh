90dc7e3a3779ec2517c30b972f2c46efc19d2706
Florian Fischer <florian.fl.fischer@fau.de>
2016-01-11 16:50:10 +0100

    make i3bar volume info work with pamixer or amixer
    1) change conkyrc file
    2) add script that prints a status line follwing this scheme:
        M|U Volume:|<default pa device> [<volume>%]

---
+++ scripts/audio_status.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+function print_mute {
+   echo -n "M " 
+}
+
+function print_unmute {
+   echo -n "U " 
+}
+
+if type pamixer &>/dev/null
+then
+    if pamixer --get-mute &> /dev/null
+    then
+        print_mute
+    else
+       print_unmute
+   fi
+
+   echo -n $(pacmd info | grep 'Default sink' | grep -o '[^.]*$')
+
+   echo " [$(pamixer --get-volume)%]"
+
+elif type amixer &>/dev/null
+then
+    if [[ "$(amixer sget Master | grep '\[on\]')" == "" ]]
+    then
+        print_mute
+    else
+        print_unmute
+    fi
+
+    echo -n "Volume: "
+
+    echo "[$(amixer sget Master | grep Mono: | sed -e 's/[ \t]*Mono: Playback [0-9]* \[\([0-9]*%\)\].*/\1/')]"
+else
+    echo "please install either pamixer or amixer"
+fi
