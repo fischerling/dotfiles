9b468fc9bbac3ae8cb72ac3fc6c0419cdddf036e
Florian Fischer <florian.fl.fischer@fau.de>
2016-04-21 15:46:13 +0200

    use $HOME instead of hardcoded directory

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,22 +1,22 @@
 # autoload functions and completions in .dotfiles 
-set fish_function_path /home/fischerling/.dotfiles/config/fish/functions $fish_function_path
-set fish_completion_path /home/fischerling/.dotfiles/config/fish/completions $fish_completion_path
+set fish_function_path $HOME/.dotfiles/config/fish/functions $fish_function_path
+set fish_completion_path $HOME/.dotfiles/config/fish/completions $fish_completion_path
 
 
 # add git-alias subdirectory to functions path
-set fish_function_path /home/fischerling/.dotfiles/config/fish/functions/git-alias $fish_function_path
+set fish_function_path $HOME/.dotfiles/config/fish/functions/git-alias $fish_function_path
 
 # include wd
-set fish_function_path /home/fischerling/.dotfiles/config/fish/wd/functions $fish_function_path
-set fish_complete_path /home/fischerling/.dotfiles/config/fish/wd/completions $fish_complete_path
+set fish_function_path $HOME/.dotfiles/config/fish/wd/functions $fish_function_path
+set fish_complete_path $HOME/.dotfiles/config/fish/wd/completions $fish_complete_path
 
 # include mark
-set fish_function_path /home/fischerling/.dotfiles/config/fish/mark/functions $fish_function_path
-set fish_complete_path /home/fischerling/.dotfiles/config/fish/mark/completions $fish_complete_path
+set fish_function_path $HOME/.dotfiles/config/fish/mark/functions $fish_function_path
+set fish_complete_path $HOME/.dotfiles/config/fish/mark/completions $fish_complete_path
 
 # include fink
-set fish_function_path /home/fischerling/.dotfiles/config/fish/fink/functions $fish_function_path
-set fish_complete_path /home/fischerling/.dotfiles/config/fish/fink/completions $fish_complete_path
+set fish_function_path $HOME/.dotfiles/config/fish/fink/functions $fish_function_path
+set fish_complete_path $HOME/.dotfiles/config/fish/fink/completions $fish_complete_path
 
 if not set -q __fish_git_prompt_show_informative_status
     set -Ux __fish_git_prompt_show_informative_status true
@@ -35,7 +35,7 @@ if not set -q BROWSER
 end
 
 if not set -q SSH_KEY_PATH
-    set -Ux SSH_KEY_PATH /home/fischerling/.ssh/rsa_id
+    set -Ux SSH_KEY_PATH $HOME/.ssh/rsa_id
 end
 
 if not contains /usr/local/bin $PATH
