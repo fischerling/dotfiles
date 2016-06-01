59d37ada838e8233a65c4527c79c8624a4cb0ee6
Florian Fischer <florian.fl.fischer@fau.de>
2016-06-01 19:28:25 +0200

    include a password file for mutt and other misc changes
    config.fish:    1) include ~/.local/bin in the PATH variable
                    2) prepend new PATH components
    
    muttrc:         1) write comment about new password file
                    2) use variables exported by mutt.sh
                    3) use dmenu instead of rofi to select links from urlscan
    
    mutt.sh:        1) mutt wrapper script that decrypts and reads the password file
                    2) start mutt dependend on the calling context (Termial|XSession)
    
    password:       new password file. The Layout is explained in muttrc
    
    setup.py:       include mutt.sh in the mutt target

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,14 +6,6 @@ set fish_complete_path $HOME/.dotfiles/config/fish/completions $fish_complete_pa
 # add git-alias subdirectory to functions path
 set fish_function_path $HOME/.dotfiles/config/fish/functions/git-alias $fish_function_path
 
-# include cargo binaries
-if type -q multirust
-    for p in $HOME/.multirust/toolchains/*/cargo/bin
-        if not contains $p $PATH
-            set PATH $PATH $p
-        end
-    end
-end
 
 if not set -q __fish_git_prompt_show_informative_status
     set -Ux __fish_git_prompt_show_informative_status true
@@ -35,6 +27,21 @@ if not set -q SSH_KEY_PATH
     set -Ux SSH_KEY_PATH $HOME/.ssh/rsa_id
 end
 
+# PATH Stuff
+
+# include cargo binaries
+if type -q multirust
+    for p in $HOME/.multirust/toolchains/*/cargo/bin
+        if not contains $p $PATH
+            set PATH $PATH $p
+        end
+    end
+end
+
+if not contains ~/.local/bin $PATH
+    set PATH ~/.local/bin $PATH 
+end
+
 if not contains /usr/local/bin $PATH
-    set PATH $PATH /usr/local/bin
+    set PATH /usr/local/bin $PATH 
 end
