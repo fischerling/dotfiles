db18bd1f4b1ecadc91d14fafe29926d4a47a8d43
Florian Fischer <muhq@muhq.space>
2017-05-14 16:56:09 +0200

    improve guixsd config

--- guix/system.scm
+++ guix/system.scm
@@ -1,58 +1,94 @@
 (use-modules (gnu) (gnu system nss))
-(use-service-modules desktop)
-(use-package-modules base wm certs text-editors version-control shells admin file xorg video gnupg ssh linux)
+(use-service-modules 
+	base
+	desktop
+	xorg
+	networking
+	dbus
+	)
+
+(use-package-modules
+;;	base
+	gcc
+	python
+	certs
+	wm ;; i3
+	conky
+	suckless ;; dmenu
+	text-editors ;; vis
+	version-control ;; git
+	shells ;; fish
+	admin ;; htop
+	file ;; file
+	video
+	gnupg
+	ssh
+	linux ;; alsa-utils
+	)
 
 (operating-system
-  (host-name "fishbowl")
-  (timezone "Europe/Berlin")
-  (locale "en_US.UTF-8")
+	(host-name "fishbowl")
+	(timezone "Europe/Berlin")
+	(locale "en_US.UTF-8")
+	
+	(bootloader (grub-configuration (device "/dev/sda")))
+	
+	(mapped-devices
+		(list (mapped-device
+			(source (uuid "47c46f56-bdf9-4982-a00e-4aaf8f3b969f"))
+			(target "the-root-device")
+			(type luks-device-mapping))))
 
-  (bootloader (grub-configuration (device "/dev/sda")))
+	(file-systems (cons*
+		(file-system
+			(device "root")
+			(title 'label)
+			(mount-point "/")
+			(type "ext4")
+			(dependencies mapped-devices))
+		%base-file-systems))
 
-  (mapped-devices
-   (list (mapped-device
-          (source (uuid "47c46f56-bdf9-4982-a00e-4aaf8f3b969f"))
-          (target "the-root-device")
-          (type luks-device-mapping))))
+	(swap-devices '("/swap"))
 
-  (file-systems (cons* (file-system
-                        (device "root")
-                        (title 'label)
-                        (mount-point "/")
-                        (type "ext4")
-                        (dependencies mapped-devices))
-                      %base-file-systems))
-  (swap-devices '("/swap"))
+	(users (cons
+		(user-account
+			(name "fischerling")
+			(comment "")
+			(group "users")
+			(supplementary-groups '("wheel" "netdev"
+						"audio" "video"))
+			(home-directory "/home/fischerling")
+			(shell #~(string-append #$fish "/bin/fish")))
+		%base-user-accounts))
 
-  (users (cons (user-account
-                (name "fischerling")
-                (comment "")
-                (group "users")
-                (supplementary-groups '("wheel" "netdev"
-                                        "audio" "video"))
-                (home-directory "/home/fischerling"))
-                ;;(shell "fish"))
-               %base-user-accounts))
+	(packages (cons*
+		i3-wm i3status dmenu conky ;; Desktop
+		
+		gcc python-wrapper git
+		fish
+		file
+		vis
+		htop
+		gnupg
+		openssh
+		alsa-utils
+		nss-certs               ;for HTTPS access
+		%base-packages))
 
-  (packages (cons* i3-wm                   ;window managers
-                   git
-                   fish
-                   wpa-supplicant
-                   acpi
-                   file
-                   vis
-                   htop
-                   gnupg
-                   openssh
-                   xorg-server
-                   xinit
-                   mpv
-                   youtube-dl
-                   nss-certs               ;for HTTPS access
-                   %base-packages))
+	(services (cons* 
+		(console-keymap-service "de")
 
-  (services (cons* (console-keymap-service "de")
-                   %base-services))
+		(slim-service )
+		
+		(wicd-service)
+		
+		(dbus-service)
+		(polkit-service)
+		(upower-service)
+		(udisks-service)
+		(colord-service)
+		(ntp-service)
+		%base-services))
 
-  ;; Allow resolution of '.local' host names with mDNS.
-  (name-service-switch %mdns-host-lookup-nss))
+	;; Allow resolution of '.local' host names with mDNS.
+	(name-service-switch %mdns-host-lookup-nss))
