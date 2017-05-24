b03002c04c44a390f59ad1e0d71a1174d5d0d3ca
Florian Fischer <muhq@muhq.space>
2017-05-24 12:35:27 +0200

    make conky do what I want, finally :)

--- guix/packages/own/conky-wl.scm
+++ guix/packages/own/conky-wl.scm
@@ -1,14 +1,17 @@
 (define-module (own conky-wl)
     #:use-module (gnu packages conky)
-    #:use-module (guix build-system cmake)
-    #:use-module (guix packages))
+    #:use-module (gnu packages linux) ;wireless-tools
+    #:use-module (guix packages)
+    #:use-module (guix utils))
 
 (define-public conky-wl
     (package
         (inherit conky)
         (name "conky-wl")
         (synopsis "conky with wireless support" )
-        (build-system cmake-build-system)
-        (arguments `(
-            #:configure-flags '("-DBUILD_WLAN=true")
-            ,@(package-arguments conky)))))
+	(inputs `(
+         ("wireless-tools" ,wireless-tools)
+         ,@(package-inputs conky)))
+        (arguments
+	 (substitute-keyword-arguments (package-arguments conky)
+          ((#:configure-flags cf) `(cons "-DBUILD_WLAN=true" ,cf))))))
