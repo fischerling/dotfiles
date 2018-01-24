b89d47b6a43f13f143d0d110cd6610ae1d691cf8
Florian Fischer <florian.fl.fischer@fau.de>
2018-01-24 03:17:10 +0100

    [qutebrowser] add key binding for password fill

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -4,4 +4,6 @@ import os
 config.bind('x', 'spawn mpv {url}')
 config.bind(',x', 'hint links spawn mpv {hint-url}')
 
+config.bind('<Ctrl-l>', 'spawn --userscript password_fill')
+
 c.editor.command = [os.environ["TERMINAL"], "-e", "vim", "{}"]
