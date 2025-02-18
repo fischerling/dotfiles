8c503d17cd220bede2c57191af29fd36a8b95c6c
Florian Fischer <florian.fischer@muhq.space>
2025-02-18 15:35:14 +0100

    [vis] only use inotify if ionotifywait is installed

--- config/vis/visrc.lua
+++ config/vis/visrc.lua
@@ -96,9 +96,11 @@ vis.events.subscribe(vis.events.INIT, function()
 end)
 
 local inotify = require('plugins/vis-inotify')
-inotify.add_recreate_watch('/home/muhq/.config/vis/themes/theme.lua', function(_)
-  vis:command("set theme " .. theme) -- reload vis theme
-end, '-P')
+if inotify.add_recreate_watch then
+  inotify.add_recreate_watch('/home/muhq/.config/vis/themes/theme.lua', function(_)
+    vis:command("set theme " .. theme) -- reload vis theme
+  end, '-P')
+end
 
 -- seperate default settings into per file and per window to not override settings from
 -- vis-editorconf which are set on FILE_OPEN
